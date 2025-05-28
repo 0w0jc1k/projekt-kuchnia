acab313d877744f58b2fcb157970387e09d2967d
0w0jc1k <284509@student.pwr.edu.pl>
2025-05-28 17:26:47 +0200

    Initial commit - added base classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Configuration config = new Configuration(1,1,5);
+        Configuration config = new Configuration(1,1,1);
 
         Simulation simulation = new Simulation(config);
         simulation.initialize(); //inicjalizuje liczbe klientow i kucharzy
